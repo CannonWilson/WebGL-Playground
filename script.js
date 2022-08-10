5f6704091d917fb67e4d2f6290e44cbcb0552bd7
CannonWilson <CannonGWilson@gmail.com>
2022-08-10 13:37:23 -0500

    Attributes simplified (no buffer)

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const vertexShaderSource = `#version 300 es
+precision mediump float;
 
+layout(location = 0) in vec4 aPosition;
 layout(location = 1) in float aPointSize;
-layout(location = 0) in vec2 aPosition;
-layout(location = 2) in vec3 aColor;
+layout(location = 2) in vec4 aColor;
 
-out vec3 vColor;
+out vec4 vColor;
 
 void main()
 {
 	vColor = aColor;
 	gl_PointSize = aPointSize;
-	gl_Position = vec4(aPosition, 0.0, 1.0);
+	gl_Position = aPosition;
 }`
 
 const fragmentShaderSource = `#version 300 es
@@ -18,11 +19,11 @@ precision mediump float;
 
 out vec4 fragColor;
 
-in vec3 vColor;
+in vec4 vColor;
 
 void main()
 {
-	fragColor = vec4(vColor, 1.0);
+	fragColor = vColor;
 }`
 
 const canvas = document.querySelector('canvas')
@@ -46,28 +47,14 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 	console.log(gl.getShaderInfoLog(fragmentShader))
 }
 
-gl.useProgram(program);
-
-const bufferData = new Float32Array([
-	  0,   0,   100,   1,0,0,
-	 .5, -.8,    32,   0,1,0,
-	-.9,  .5,    50,   0,0,1,
-])
+gl.useProgram(program)
 
 const aPositionLoc = 0 // gl.getAttribLocation(program, 'aPosition')
 const aPointSizeLoc = 1 // gl.getAttribLocation(program, 'aPointSize')
 const aColorLoc = 2 // gl.getAttribLocation(program, 'aColor')
 
-gl.enableVertexAttribArray(aPositionLoc)
-gl.enableVertexAttribArray(aPointSizeLoc)
-gl.enableVertexAttribArray(aColorLoc)
+gl.vertexAttrib4f(aPositionLoc, 0, 0, 0, 1)
+gl.vertexAttrib1f(aPointSizeLoc, 100)
+gl.vertexAttrib4f(aColorLoc, 1, 0, 0, 1)
 
-const buffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW)
-
-gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, 6 * 4, 0)
-gl.vertexAttribPointer(aPointSizeLoc, 1, gl.FLOAT, false, 6 * 4, 2 * 4)
-gl.vertexAttribPointer(aColorLoc, 3, gl.FLOAT, false, 6 * 4, 3 * 4)
-
-gl.drawArrays(gl.TRIANGLES, 0, 3)
+gl.drawArrays(gl.POINTS, 0, 3)
